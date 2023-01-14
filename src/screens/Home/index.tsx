ee0fda74323410e4a9933f120a123bee9181970e
DanielFrancajp <danieldenotas@gmail.com>
2023-01-14 16:08:51 -0300

    branch principal

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,39 @@
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { styles } from './styles'
+
+import { Participant } from '../../components/Participant'
+
+export default function Home() {
+
+    function handleParticipantAdd() {
+        console.log("Você clicou no botão de adicionar !")
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName}
+            > Nome do evento
+            </Text>
+            <Text style={styles.eventDate}>
+                Quinta-feira, 12 de Janeiro de 2023
+            </Text>
+
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Nome do participante"
+                    placeholderTextColor="#6B6B6B"
+                />
+
+                <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+                    <Text style={styles.buttonText}>
+                        +
+                    </Text>
+                </TouchableOpacity>
+            </View>
+
+        <Participant/>
+        <Participant/>
+        </View>
+    )
+}
